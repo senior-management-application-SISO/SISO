7807508045dbfa0bb157a925ba94e0cd19fdbb42
혁키 <dnwnsgur12@gmail.com>
2022-10-18 13:55:06 +0900

    update

--- src/test/java/siso/project/repository/CountyOfficeMapperTest.java
+++ src/test/java/siso/project/repository/CountyOfficeMapperTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+@Rollback(value = true)
 class CountyOfficeMapperTest {
 
     @Autowired
