c04e30ef305a875498bc62fe28e598a83bcf7af5
woohyeonjoe <qlqkthd8802@gmail.com>
2022-10-18 10:46:34 +0900

    userStateClass-update

--- src/main/java/siso/project/repository/UsersStateMapper.java
+++ src/main/java/siso/project/repository/UsersStateMapper.java
@@ -2,19 +2,17 @@ package siso.project.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import siso.project.domain.Users;
-import siso.project.domain.UsersQrState;
-import siso.project.repository.dto.UsersDto;
+import siso.project.domain.UsersState;
 import siso.project.repository.dto.UsersQrStateDto;
 
 import java.util.List;
 import java.util.Optional;
 
 @Mapper
-public interface UsersQrStateMapper {
+public interface UsersStateMapper {
 
     //저장
-    void save(UsersQrState usersQrState);
+    void save(UsersState usersState);
 
     //업데이트
     void update(@Param("id") Long id, @Param("updateParam") UsersQrStateDto usersQrStateDto);
@@ -23,9 +21,9 @@ public interface UsersQrStateMapper {
     void delete(Long id);
 
     //검색
-    List<UsersQrState> select(UsersQrStateDto usersQrStateDto);
+    List<UsersState> select(UsersQrStateDto usersQrStateDto);
 
 
     //단건조회
-    Optional<UsersQrState> findById(Long id);
+    Optional<UsersState> findById(Long id);
 }
