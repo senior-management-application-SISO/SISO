1b7dc8cea44da5294917b171ea2d3770d18f546d
woohyeonjoe <qlqkthd8802@gmail.com>
2022-10-21 15:59:51 +0900

    villagehall-save

--- src/main/java/siso/project/controller/villagehall/VillageHallController.java
+++ src/main/java/siso/project/controller/villagehall/VillageHallController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import siso.project.domain.Admin;
+import siso.project.domain.Teams;
 import siso.project.domain.VillageHall;
 import siso.project.repository.dto.TeamsDto;
 import siso.project.service.VillageHallService;
@@ -25,6 +27,18 @@ public class VillageHallController {
         List<VillageHall> villageHalls = villageHallService.villageHallSelect(loginAdmin.getId(), cond);
         model.addAttribute("villageHalls", villageHalls);
         return "villagehall/villageHalls";
+    }
+
+    @GetMapping("/villagehall/save")
+    public String villageHallFormView(Model model) {
+        model.addAttribute("villageHall", VillageHall.builder().build());
+        return "villagehall/addVillageHallForm";
+    }
+
+    @PostMapping("/villagehall/save")
+    public String villageHallSave(@SessionAttribute(name = SessionConst.LOGIN_ADMIN, required = false) Admin loginAdmin, @ModelAttribute VillageHall villageHall) {
+        villageHallService.villageHallSave(loginAdmin.getId(), villageHall);
+        return "redirect:/villagehall";
     }
 
 }
