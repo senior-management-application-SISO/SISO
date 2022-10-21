dbbd3c3e13561190819943c791b6c6ab25fd3b95
woohyeonjoe <qlqkthd8802@gmail.com>
2022-10-21 14:26:56 +0900

    villagehall

---
+++ src/main/java/siso/project/service/VillageHallService.java
@@ -0,0 +1,12 @@
+package siso.project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import siso.project.repository.VillageHallMapper;
+
+@Service
+@RequiredArgsConstructor
+public class VillageHallService {
+
+    private static VillageHallMapper villageHallMapper;
+}
